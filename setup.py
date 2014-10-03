d156c8c5863c536ce5b6cc724d3e4d297b967030
Florent Messa <florent.messa@gmail.com>
2014-10-03 11:44:40 +0200

    Make setup.py pass the PEP8

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup
+
 import os.path
 
 setup(
-    name = 'ulule',
-    version = '0.1',
-    author = 'Damien Cirotteau',
-    author_email = 'dcirotteau@ctrl-x.fr',
-    description = 'A Python API library for the Ulule crowdfunding platform.',
-    long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
-    license = 'MIT',
-    keywords = 'ulule crowdfunding api',
-    url = 'https://github.com/ulule/ulule-python',
-    py_modules = ['ulule'],
-    install_requires = ['requests >= 0.13.2',],
-    classifiers = [
+    name='ulule-python',
+    version='0.1',
+    author='Damien Cirotteau',
+    author_email='dcirotteau@ctrl-x.fr',
+    description='A Python API library for the Ulule crowdfunding platform.',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    license='MIT',
+    keywords='ulule crowdfunding api',
+    url='https://github.com/ulule/ulule-python',
+    py_modules=['ulule'],
+    install_requires=['requests >= 0.13.2', ],
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
